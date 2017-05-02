3b4aa145988d0ccbef1740c553d86a8e7eb91dce
Dariusz Prochownik <dariusz.prochownik@gmail.com>
2017-05-02 12:15:24 +0200

    first commit

---
+++ Gim_Lab6/Assets/Scripts/DoorBehaviour.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorBehaviour : MonoBehaviour
+{
+    public GameObject Door;
+    private Animator _animator;
+	// Use this for initialization
+	void Start ()
+	{
+	    _animator = Door.GetComponent<Animator>();
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _animator.SetBool("Open", true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _animator.SetBool("Open", false);
+        }
+    }
+}
---
+++ Gim_Lab6/Assets/Scripts/PlayerPoints.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerPoints : MonoBehaviour
+{
+
+    public GameObject[] Walls;
+    private GameObject _winningWall;
+    public Text PointsText;
+    private int _poinCount;
+
+    // Use this for initialization
+    void Start ()
+    {
+        RandomNewWinningWall();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("DoorInsides"))
+        {
+            var door = other.gameObject.transform.parent.gameObject;
+            UpdatePoints(door == _winningWall ? 1 : -1);
+            if(door == _winningWall)
+                RandomNewWinningWall();
+        }
+    }
+
+    private void RandomNewWinningWall()
+    {
+        _winningWall = Walls[Random.Range(0, Walls.Length)];
+    }
+
+    private void UpdatePoints(int addPointCount)
+    {
+        _poinCount += addPointCount;
+        PointsText.text = "Points: " + _poinCount;
+    }
+}
